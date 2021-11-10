c6def2354dff1e186c292a23b4e765b9a75c87c1
chc <103759478@qq.com>
2021-11-10 17:17:48 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const koa = require('koa');
+const app = new koa();
+const Router = require('koa-router')
+const router = new Router();
+router.get('/hello',(ctx,next)=>{
+	ctx.body = {code:1,message:'请求成功',query:ctx.request.query,querystring:ctx.request.querystring}
+})
+app
+.use(router.routes())
+app.listen(3000,()=>{
+	console.log('server is running at http://localhost:3000')
+})
